00a443143364fe7de17b97a5643445a04d9eedfe
Daniel Fajtai <daniel.fajtai@gmail.com>
2025-05-28 14:21:23 +0200

    Spring Boot backend initialized with Spring Initializr

---
+++ backend/src/main/java/com/s4/small_scale_shift_scheduler/SmallScaleShiftSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.s4.small_scale_shift_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallScaleShiftSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallScaleShiftSchedulerApplication.class, args);
+	}
+
+}
