cb7be009a156846ab1f837112cdf2f2847def00a
Daniel Fajtai <daniel.fajtai@gmail.com>
2025-05-28 14:21:28 +0200

    Create SmallScaleShiftSchedulerApplicationTests.java

---
+++ backend/src/test/java/com/s4/small_scale_shift_scheduler/SmallScaleShiftSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s4.small_scale_shift_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallScaleShiftSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
